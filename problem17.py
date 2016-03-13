ed759a275a5be198db68f290305c2c1e55676ca6
Tyler <tlee753@gmail.com>
2016-03-13 18:33:36 -0400

    Solved.

--- problem17.py
+++ problem17.py
@@ -1,3 +1,12 @@
+"""
+Euler Problem 17
+================
+Iterates through every number from one to one thousand, counting the number of letters
+
+- Created by tlee753
+- Last Modified 3/13/16
+"""
+
 import math
 
 count = 0
@@ -8,30 +17,29 @@ onesArray = [0, 3, 3, 5, 4, 4, 3, 5, 5, 4, 3, 6, 6, 8, 8, 7, 7, 9, 8, 8]
 tensArray = [0, 3, 6, 6, 5, 5, 5, 7, 6, 6]
 # zero, ten, twenty, thirty
 
-for i in range(1,102):
+for i in range(1,1001):
 # iterates through all number from 'one' to 'one thousand'
+
     string = str(i)
     # converts the number into a string
 
     if len(string) == 1:
+    # one digit numbers
         count += onesArray[i]
     
     if len(string) == 2:
+    # two digit numbers
 
         if i < 20:
             count += onesArray[i]
         else:
-            firstDigit = int(string[0])
-            secondDigit = int(string[1])
-            count += tensArray[firstDigit]
-            count += onesArray[secondDigit]
+            count += tensArray[int(string[0])]
+            count += onesArray[int(string[1])]
 
     if len(string) == 3:
-        firstDigit = int(string[0])
-        secondDigit = int(string[1])
-        thirdDigit = int(string[2])
-
-        count += onesArray[firstDigit]
+    # three digit numbers
+       
+        count += onesArray[int(string[0])]
         # adds ones digit that starts a three digit number
         
         if i % 100 == 0:
@@ -41,13 +49,15 @@ for i in range(1,102):
         count += 10
         # hard code for 'hundred' and 'and'
 
-        if int(string[1:2]) < 20:
-            count += onesArray[int(string[1:2])]
+        if int(string[1:3]) < 20:
+            count += onesArray[int(string[1:3])]
         else:
-            count += tensArray(secondDigit)
-            count += onesArray(thirdDigit)
+            count += tensArray[int(string[1])]
+            count += onesArray[int(string[2])]
 
     if len(string) == 4:
+    # four digit numbers
+
         count += 11
         # hard code for 'one thousand'
 
