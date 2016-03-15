3ab52d400d9a5acafca981adcea828edae0e5e97
Tyler <tlee753@gmail.com>
2016-03-15 13:39:52 -0400

    Starting Project 31

---
+++ problem31.py
@@ -0,0 +1,10 @@
+"""
+Euler Problem 31
+================
+Finds the number of combinations of British currency that can be used to make two pounds
+
+- Created by Tyler Lee
+- Last Modified 3/12/16
+"""
+
+import math
