9b0a1e84714c4e5be3f17b5cec5f8a90d35a192b
Tyler Lee <tlee753@gmail.com>
2016-04-04 09:01:21 -0400

    Ideation

---
+++ problem04.py
@@ -0,0 +1,20 @@
+"""
+Euler Problem 4
+===============
+
+- created by tlee753
+- last modified 4.5.16
+"""
+
+i = 0
+
+while (i <= 999*999):
+    num = str(i)
+    size = len(num)
+    while (string[0] == string[size - 1] and size > 1)
+        string = string
+        size -= 2
+        ans = 
+    i += 1
+
+print ans
