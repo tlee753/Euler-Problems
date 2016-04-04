c20272f90e1b066a0149f9145bf81c6e9322a8bf
Tyler Lee <tlee753@gmail.com>
2016-04-03 22:21:53 -0400

    Solved

--- problem03.py
+++ problem03.py
@@ -7,7 +7,7 @@ Find the greatest factor for a given number
 - last modified 4.4.16
 """
 
-n = 50 # initialize the number in question
+n = 600851475143 # initialize the number in question
 i = 3 # initialize the smallest integer factor greater than 2
 
 while (n % 2 == 0): # remove all factors of two
@@ -20,4 +20,4 @@ while (n != 1):
     i += 2
 
 
-print 'The greatest prime factor is ', ans
+print ans
